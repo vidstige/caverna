4e1ad5ef432d8d82b01b182c23e10284da21f226
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-02-06 22:18:46 +0100

    place dwarfs

--- caverna.py
+++ caverna.py
@@ -1,27 +1,52 @@
+import ai
 
 class Player(object):
     def __init__(self, name):
         self.name = name
-        self.points = 0
+        self.dwarfs = [0, 0]
+        self.resources = dict(food=2)
+
+
+class Action(object):
+    def __init__(self, name, r):
+        self.name = name
+        self.resources = r
 
 
 class Game(object):
     def __init__(self, players):
         self.players = players
-        self.counter = 0
+        self.actions = [Action(name="Drift Mining", r=dict(stones=1))]
+        self.turn = 0
+        self.dwarfs = {}  # placed dwarfs
 
     def over(self):
-        return True
+        return self.turn > 8
+
+    def score(self, player):
+        return 0
 
 
 def main():
-    players = [Player("samuel"), Player("maria")]
-    game = Game(players)
+    samuel = Player("samuel")
+    maria = Player("maria")
+    controllers = {samuel: ai.Random(samuel), maria: ai.Random(maria)}
+    game = Game([samuel, maria])
     while not game.over():
+        print("Turn {}".format(game.turn))
+
         # Take actions
         for player in game.players:
-            pass
-        
+            if player.dwarfs:
+                dwarf = player.dwarfs.pop()
+                action = controllers[player].select_action()
+                game.dwarfs[action] = (player, dwarf)
+
+        # Return dwarfs
+        for player, dwarf in game.dwarfs.values():
+            player.dwarfs.append(dwarf)
+        game.dwarfs = {}
+
         # Replenish resources
 
         # Harvest crops
@@ -31,6 +56,13 @@ def main():
             pass
 
         # Breed animals
+
+        # Next turn
+        game.turn += 1
+
+    for player in game.players:
+        score = game.score(player)
+        print("{name}: {score}".format(name=player.name, score=score))
 
 
 if __name__ == "__main__":
