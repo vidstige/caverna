f232ef4830393c2048b426f1dcfa71ca9ea1856b
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-02-07 09:27:28 +0100

    moving out stuff to game.state

--- caverna.py
+++ caverna.py
@@ -1,11 +1,9 @@
 import ai
 
 class Player(object):
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
         self.dwarfs = [0, 0]
         self.resources = dict(food=2)
-
 
 class Action(object):
     def __init__(self, name, r):
@@ -14,29 +12,38 @@ class Action(object):
 
 
 class Game(object):
+    class State(object):
+        """Encompasses and entire game state"""
+        def __init__(self, players):
+            self.players = players
+            self.round = 0
+
+
     def __init__(self, players):
-        self.players = players
         self.actions = [Action(name="Drift Mining", r=dict(stones=1))]
-        self.turn = 0
+        self.state = Game.State(players)
         self.dwarfs = {}  # placed dwarfs
 
+    def players(self):
+        return self.state.players.values()
+
     def over(self):
-        return self.turn > 8
+        return self.state.round > 12
 
     def score(self, player):
         return 0
 
 
 def main():
-    samuel = Player("samuel")
-    maria = Player("maria")
+    samuel = Player()
+    maria = Player()
     controllers = {samuel: ai.Random(samuel), maria: ai.Random(maria)}
-    game = Game([samuel, maria])
+    game = Game({'samuel': samuel, 'maria': maria})
     while not game.over():
-        print("Turn {}".format(game.turn))
+        print("Round {}".format(game.state.round))
 
         # Take actions
-        for player in game.players:
+        for player in game.players():
             if player.dwarfs:
                 dwarf = player.dwarfs.pop()
                 action = controllers[player].select_action()
@@ -52,17 +59,17 @@ def main():
         # Harvest crops
 
         # Feed dwarfs
-        for player in game.players:
+        for player in game.players():
             pass
 
         # Breed animals
 
         # Next turn
-        game.turn += 1
+        game.state.round += 1
 
-    for player in game.players:
+    for name, player in game.state.players.items():
         score = game.score(player)
-        print("{name}: {score}".format(name=player.name, score=score))
+        print("{name}: {score}".format(name=name, score=score))
 
 
 if __name__ == "__main__":
