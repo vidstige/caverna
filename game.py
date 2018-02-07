d49f8cf105cdd659b05f0ae5f7ec4a1322b07d38
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-02-07 13:02:18 +0100

    refactor: move out function

--- game.py
+++ game.py
@@ -1,5 +1,42 @@
+from typing import List
 from caverna import *
 import ai
+
+
+def play(game: Game, controllers: List[Controller]) -> None:
+    # Replenish resources (this is done first to ensure initial supply)
+    game.replenish()
+
+    # Take actions while any player has dwarfs
+    while any(p.dwarfs for p in game.players.values()):
+        for name, player in game.players.items():
+            if player.dwarfs:
+                dwarf = player.dwarfs.pop()
+                action = controllers[player].select_action(game)
+                print("{} selects {}".format(name, action.name))
+                
+                # place dwarf
+                game.state.dwarfs[action] = (player, dwarf)
+                
+                # gain resources
+                game.gain_resources(action, player)
+
+                # place tile
+                pass
+
+    # Return dwarfs
+    game.return_dwarfs()
+
+    # Harvest crops
+    pass
+
+    # Feed dwarfs
+    for player in game.state.players:
+        pass
+
+    # Breed animals
+    pass
+
 
 def main():
     samuel = Player()
@@ -8,36 +45,7 @@ def main():
     game = Game({'samuel': samuel, 'maria': maria})
     while not game.over():
         print("Round {}".format(game.state.round))
-
-        # Replenish resources (this is done first to ensure initial supply)
-        game.replenish()
-
-        # Take actions while any player has dwarfs
-        while any(p.dwarfs for p in game.players.values()):
-            for name, player in game.players.items():
-                if player.dwarfs:
-                    dwarf = player.dwarfs.pop()
-                    action = controllers[player].select_action(game)
-                    print("{} selects {}".format(name, action.name))
-                    
-                    # place dwarf
-                    game.state.dwarfs[action] = (player, dwarf)
-                    
-                    # gain resources
-                    game.gain_resources(action, player)
-
-        # Return dwarfs
-        game.return_dwarfs()
-
-        # Harvest crops
-
-        # Feed dwarfs
-        for player in game.state.players:
-            pass
-
-        # Breed animals
-
-        # Next turn
+        play(game, controllers)
         game.state.round += 1
 
     for name, player in game.players.items():
