21b21e3cecfeefdb030d4c921594e2ffc1c206d7
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-02-07 09:30:21 +0100

    random ai

---
+++ ai.py
@@ -0,0 +1,11 @@
+class Controller(object):
+    def __init__(self, player):
+        self.player = player
+
+    def select_action(self):
+        return None
+
+class Random(Controller):
+    def select_action(self):
+        return None
+
