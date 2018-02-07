2d268f3a534a09fe064f8a992af3fa0cfac2d178
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-02-07 10:19:22 +0100

    actually random ai

--- ai.py
+++ ai.py
@@ -1,5 +1,6 @@
+import random
 from caverna import Controller, available_actions
 
 class Random(Controller):
     def select_action(self, game):
-        return available_actions(game)[0]
+        return random.choice(available_actions(game))
